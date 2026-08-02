50453f27e639a114cedd31345c41f8ddbe8b8946
luwamasmelash <liwix24@gmail.com>
2026-08-02 18:46:59 +0300

    backend updated

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import imagekit from '../configs/imageKit.js';
 import Blog from '../models/Blog.js';
 import Comment from '../models/Comment.js';
-import main from '../configs/gemini.js';
+import main from '../configs/openRouter.js';
 
 export const addBlog = async (req, res) => {
     try {

a9b48d93b169af562c2fb90baa8e2225d5968063
luwamasmelash <liwix24@gmail.com>
2026-08-03 08:59:32 +0300

    imagekit test

--- backend/configs/imageKit.js
+++ backend/configs/imageKit.js
@@ -1,9 +1,13 @@
 import ImageKit from "imagekit";
 
+console.log("PUBLIC:", process.env.IMAGEKIT_PUBLIC_KEY);
+console.log("PRIVATE:", process.env.IMAGEKIT_PRIVATE_KEY ? "FOUND" : "MISSING");
+console.log("URL:", process.env.IMAGEKIT_URL_ENDPOINT);
+
 const imagekit = new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
 export default imagekit;
